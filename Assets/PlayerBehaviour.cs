35128bd78bec71c86d5f53ee6a4afcb01b795310
SUCHV\suchv <admix228@gmail.com>
2023-10-08 14:01:41 +0200

    Paddle movement script added

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+
+    void Update()
+    {
+        Vector2 inputVector = new Vector2(0,0);
+
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1;
+        }
+
+        Vector3 moveDir = new Vector3(inputVector.x,0f,0f);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        Debug.Log(inputVector);
+    }
+}
