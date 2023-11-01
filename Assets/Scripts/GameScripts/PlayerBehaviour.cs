de101dd3884aa3fe82a0b9d66139b3c1f689857a
SUCHV\suchv <admix228@gmail.com>
2023-11-01 13:44:13 +0100

    Player Movement limiter added

--- Assets/Scripts/GameScripts/PlayerBehaviour.cs
+++ Assets/Scripts/GameScripts/PlayerBehaviour.cs
@@ -12,7 +12,6 @@ public class PlayerBehaviour : MonoBehaviour
     //public float _mapSize;
     public float _paddleSize;
 
-
     private void Awake()
     {
         _mapDimension.x = Vector3.Distance((GameObject.Find("Env/WallLeft").transform.position), (GameObject.Find("Env/WallRight").transform.position));
@@ -24,26 +23,43 @@ public class PlayerBehaviour : MonoBehaviour
     {
         Vector2 inputVector = new Vector2(0,0);
 
-        if(Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
 
-        if(Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1;
-        }
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, 0f);
 
         // Something is really fucked up here, no idea how to fix it, gonna sleep now
-        if (Mathf.Abs(GameObject.Find("Paddle/Cube").transform.position.x) < _mapDimension.x / 2.0f)
+        if (Mathf.Abs(GameObject.Find("Paddle").transform.position.x) < _mapDimension.x / 2.0f)
         {
+            if (Input.GetKey(KeyCode.A))
+            {
+                inputVector.x = -1;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                inputVector.x = 1;
+            }
+            Vector3 moveDir = new Vector3(inputVector.x, 0f, 0f);
             transform.position += moveDir * _moveSpeed * Time.deltaTime;
 
         }
         else
         {
-            transform.position += moveDir * _moveSpeed * Time.deltaTime;
+            if((GameObject.Find("Paddle").transform.position.x>0)) {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    inputVector.x = -1;
+                }
+                Vector3 moveDir = new Vector3(inputVector.x, 0f, 0f);
+                transform.position += moveDir * _moveSpeed * Time.deltaTime;
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.D))
+                {
+                    inputVector.x = 1;
+                }
+                Vector3 moveDir = new Vector3(inputVector.x, 0f, 0f);
+                transform.position += moveDir * _moveSpeed * Time.deltaTime;
+            }
         }
     }
 }
